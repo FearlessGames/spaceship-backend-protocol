1e43815e90eec38dde67e752bc2b9248ab6bc1da
hiflyer <per.malmen@gmail.com>
2015-02-23 17:30:38 +0100

    Added simple DTO

---
+++ src/main/java/se/fearless/spaceship/api/ShipModelDTO.java
@@ -0,0 +1,13 @@
+package se.fearless.spaceship.api;
+
+public class ShipModelDTO {
+	private final String name;
+
+	public ShipModelDTO(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
